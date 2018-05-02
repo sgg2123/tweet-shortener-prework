0728953929eda323481f7f6b1c6289d715a117c3
Sydney Garay <sgg2123@columbia.edu>
2018-05-02 16:20:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ end
 def shortened_tweet_truncator(tweet)
   new_tweet = selective_tweet_shortener(tweet)
   if new_tweet.length > 140
-    new_tweet.slice(0..139)
+    new_tweet.slice(0..140)
   else
     new_tweet
   end
