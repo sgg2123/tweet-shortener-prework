1fe1fc7aaa4c9f1a751cfe0a69b88b20ee0f8e03
Sydney Garay <sgg2123@columbia.edu>
2018-05-02 15:02:07 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def dictionary
+  {:hello => 'hi', :to => '2', :two => '2', :too => '2', :for => '4', :four => '4', :be => 'b', :you => 'u', :at => "@", :and => "&"}
+end
+
+def word_substituter(tweets)
+  keys = dictionary.keys
+  words = tweets.split(' ')
+  keys.each do |key|
+    words.each do |word|
+      if word == key.to_s
+        if i = words.index(word)
+          words[i] = dictionary[word.to_sym]
+        else
+          words[i] = word
+        end
+      end
+    end
+  end
+  words.join(' ')
+end
