17eb6d56ba4b947614a843de5514b0afaf0eb596
Sydney Garay <sgg2123@columbia.edu>
2018-05-02 15:54:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,17 +2,17 @@ def dictionary
   {:hello => 'hi', :to => '2', :two => '2', :too => '2', :for => '4', :four => '4', :be => 'b', :you => 'u', :at => "@", :and => "&"}
 end
 
-def word_substituter(tweets)
+def word_substituter(tweet)
   keys = dictionary.keys
-  words = tweets.split(' ')
-  keys.map do |key|
-    words.map do |word|
-      if word == key.to_s
-        word = dictionary[key]
-      end
+  items = tweet.split(/\b/)
+  new_tweet = items.map do |item|
+    if keys.include?(item.to_sym)
+      item = dictionary[item.to_sym]
+    else
+      item
     end
   end
-  words.join(' ')
+  new_tweet.join
 end
 
 def bulk_tweet_shortener
